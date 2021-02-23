4082d1c7620c886ed2e2df487ab2bd66ad2426d6
mespinoza30 <m.espinoza16@hotmail.com>
2021-02-23 12:15:09 -0800

    Made some edits

--- src/App.js
+++ src/App.js
@@ -2,20 +2,22 @@ import './App.css';
 
 function App() {
   const name = 'Marisela';
+  const comment ='Here are the comments:'
   const post = {
-    title: "Dinosaurs are awesome",
+    title: "Dinosaurs Are Awesome",
     author: "Stealthy Stegosaurus",
     body: "Check out this body property!",
-    comments: ["First!", "Great post", "Hire this author now!"]
+    comments: ["Great title!", "Great post", "Hire this author now!"]
   }
 
   return (
     <div className="App">
       {/* can use variables with curly brackets */}
       <p>{name}</p>
-      <p>{post.title}</p>
-      <p>{post.author}</p>
+      <p><h1>Title: {post.title}</h1></p>
+      <p>Author: {post.author}</p>
       <p>{post.body}</p>
+      <p><h3>{comment}</h3></p>
       <p>{post.comments[0]}</p>
       <p>{post.comments[1]}</p>
       <p>{post.comments[2]}</p>
